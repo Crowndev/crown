227bfdb2bb300bd91cf0d6b8434f1336f7a9136f
Block Mechanic <blockmecha@gmail.com>
2022-08-11 11:46:30 +0200

    Update miner.cpp

--- src/miner.cpp
+++ src/miner.cpp
@@ -282,10 +282,14 @@ std::unique_ptr<CBlockTemplate> BlockAssembler::CreateNewBlock(const CScript& sc
         pblock->vtx.emplace_back();
         pblock->vtx[1] = MakeTransactionRef(std::move(txCoinStake));
     }
-    LogPrintf("%s: %d\n",__func__, pblock->vtx.size());
+    
+    LogPrintf("%s: vtx size %d\n",__func__, pblock->vtx.size());
+
+    if(fProofOfStake && pblock->vtx.size() > 2)
+        pblock->vtx.pop_back();
     
     for (auto a: pblock->vtx)
-        LogPrintf("%s: %s\n",__func__, a->ToString());
+        LogPrintf("%s\n", a->ToString());
 
     pblocktemplate->vchCoinbaseCommitment = GenerateCoinbaseCommitment(*pblock, pindexPrev, chainparams.GetConsensus());
     pblocktemplate->vTxFees[0] = -nFees;
