af5c3e81793755608edf5b08e5b3d956a2a931bb
Block Mechanic <blockmecha@gmail.com>
2022-10-20 22:02:55 +0200

    Update wallet.h

--- src/wallet/wallet.h
+++ src/wallet/wallet.h
@@ -1048,7 +1048,7 @@ public:
      * selected by SelectCoins(); Also create the change output, when needed
      * @note passing nChangePosInOut as -1 will result in setting a random position
      */
-    bool CreateTransaction(const std::vector<CRecipient>& vecSend, CTransactionRef& tx, CAmount& nFeeRet, int& nChangePosInOut, bilingual_str& error, const CCoinControl& coin_control, FeeCalculation& fee_calc_out, bool sign = true, const CTxDataBase& datar = CTxDataBase(), AvailableCoinsType coin_type=ALL_COINS, int extraPayloadSize = 0);
+    bool CreateTransaction(const std::vector<CRecipient>& vecSend, CTransactionRef& tx, CAmount& nFeeRet, int& nChangePosInOut, bilingual_str& error, const CCoinControl& coin_control, FeeCalculation& fee_calc_out, bool sign = true, const CTxDataBase& datar = CTxDataBase(0), AvailableCoinsType coin_type=ALL_COINS, int extraPayloadSize = 0);
     /**
      * Submit the transaction to the node's mempool and then relay to peers.
      * Should be called after CreateTransaction unless you want to abort
